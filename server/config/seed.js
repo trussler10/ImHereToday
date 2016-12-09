881cfa8c73b852c49bf6fb35ca704a374c8f6081
lmkerr <loren@lorenkerr.com>
2016-12-09 15:50:47 -0700

    added seed update for new location and cube model

--- server/config/seed.js
+++ server/config/seed.js
@@ -5,6 +5,7 @@
 
 'use strict';
 import Thing from '../api/thing/thing.model';
+import Location from '../api/location/location.model';
 
 Thing.find({}).remove()
   .then(() => {
@@ -38,3 +39,33 @@ Thing.find({}).remove()
     });
   });
 
+Location.find({}).remove()
+  .then(() => {
+    Location.create(
+    {
+      name: 'Development Tools',
+      cubes: []
+    }, {
+      name: 'Server and Client integration',
+      cubes: []
+    }, {
+      name: 'Smart Build System',
+      info: 'Build system ignores `spec` files, allowing you to keep '
+            + 'tests alongside code. Automatic injection of scripts and '
+            + 'styles into your index.html'
+    }, {
+      name: 'Modular Structure',
+      info: 'Best practice client and server structures allow for more '
+            + 'code reusability and maximum scalability'
+    }, {
+      name: 'Optimized Build',
+      info: 'Build process packs up your templates as a single JavaScript '
+            + 'payload, minifies your scripts/css/images, and rewrites asset '
+            + 'names for caching.'
+    }, {
+      name: 'Deployment Ready',
+      info: 'Easily deploy your app to Heroku or Openshift with the heroku '
+            + 'and openshift subgenerators'
+    }
+    );
+  });
